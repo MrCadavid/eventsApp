b16cccd828922fefa2cc3bc0a8644069cc774c7b
brayancadavid <bcadavid@unicesar.edu.co>
2024-11-19 19:33:40 -0500

    @feat:done list events

--- src/app/events/list/list.component.ts
+++ src/app/events/list/list.component.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject} from '@angular/core';
 import { EventUseCases } from '@application/events/use-cases/event.usecases';
 import { NotificationsUseCases } from '@application/websocket/use-cases/notifications.usecases';
 import { Event } from '@domain/events/models/event.model';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
 import { Observable } from 'rxjs';
 
 
@@ -10,13 +13,13 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardModule,TableModule,CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent{
   private readonly eventsUseCases=inject(EventUseCases);
   private readonly notifications=inject(NotificationsUseCases);
-  private readonly events$:Observable<Event[]>=this.eventsUseCases.get();
+  protected readonly events$:Observable<Event[]>=this.eventsUseCases.get();
   
 }
