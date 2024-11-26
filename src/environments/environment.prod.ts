4c4eed8527a33c5cb62d4380f888e423a7d3ba0b
Brayan Cadavid Dev <bcadavid@unicesar.edu.co>
2024-11-26 17:33:45 -0500

    @fix:url production mistaken

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   userKey: '20234a8d2574',
   featureFlag: true,
-  apiUrl: 'http://40.81.203.165:8082/',
+  apiUrl: 'http://40.81.203.165:8082',
   websocketUrl: 'ws://40.81.203.165:8082/websocket',
 };
